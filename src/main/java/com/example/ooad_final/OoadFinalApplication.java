20ad75bfa493b69e14ed0bc18c31e65119958ce0
ivanmaister7 <ivanmaister7@gmail.com>
2022-12-22 11:36:50 +0200

    Initial commit

---
+++ src/main/java/com/example/ooad_final/OoadFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ooad_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OoadFinalApplication.class, args);
+    }
+
+}
